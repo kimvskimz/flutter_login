d11b6a0d344dc6fcc7ebf6e88170b8f25a51bc0e
kimvskimz <kimvskimz@gmail.com>
2025-11-04 13:09:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import requests
+
+st.title("FitMindMove Prototype")
+st.write("스트림릿 연결 테스트입니다.")
+
+name = st.text_input("이름을 입력하세요")
+if st.button("확인"):
+    st.success(f"환영합니다, {name}님!")
 
