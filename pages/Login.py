d9ab6f7484437ad4a51d5d2466a69b1a8f2903df
kimvskimz <kimvskimz@gmail.com>
2025-11-11 14:28:51 +0900

    Update Google OAuth redirect URI

--- pages/Login.py
+++ pages/Login.py
@@ -38,7 +38,7 @@ else:
 
 if st.button("🔑 Google 로그인", key="google_login_btn"):
     GOOGLE_CLIENT_ID = "154991033089-iiim41uef7v9r01dg0g0767reom8v2cn.apps.googleusercontent.com"
-    REDIRECT_URI = "https://fitmindmove.streamlit.app/"
+    REDIRECT_URI = "https://fitmindmove.streamlit.app/pages/oauth_redirect"
     SCOPE = "email profile openid"
 
     auth_url = (
