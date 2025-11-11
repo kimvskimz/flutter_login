8f22fa68c1b69e76d5e5dd3c269cd167402416b6
kimvskimz <kimvskimz@gmail.com>
2025-11-11 14:32:45 +0900

    Delete pages/oauth_redirect.py

--- pages/oauth_redirect.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-
-params = st.experimental_get_query_params()
-if "code" in params:
-    st.session_state["auth_code"] = params["code"][0]
-    st.write("✅ Google 인증 코드 받음!")
-else:
-    st.error("❌ 인증 코드가 없습니다.")
